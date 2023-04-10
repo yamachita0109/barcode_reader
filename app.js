f19f341d4412c5b58157eb55f94859b6fbb31abc
Yamashita Kazumasa <yamachita0109@gmail.com>
2023-04-10 09:54:05 +0900

    1st

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+app.use('/public', express.static(__dirname + '/public'));
+
+
+app.get('/', async (req, res) => {
+  res.render('index')
+})
+
+app.get('/check', async (req, res) => {
+  res.json({
+    result: 'ok',
+  })
+})
+
+app.listen(process.env.API_PORT || 80)
